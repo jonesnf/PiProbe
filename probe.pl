be12472bd71a6495bb89e54f097ebc1bc9eb1b9b
jonesnf <jonesnf@miamioh.edu>
2018-05-29 10:57:51 -0600

    finished probe script. enters mac addrs into devices table with manuf. name.

--- probe.pl
+++ probe.pl
@@ -14,7 +14,6 @@ my $user = "root";
 my $pwd = "mikki";
 # connect to DB
 my $dbh = DBI->connect($dsn, $user, $pwd) or die my $DBI:errstr;
-print "$dbh\n";
 
 # Update familiar column of devices
 sub update_dev {
@@ -26,12 +25,28 @@ sub update_dev {
   #print "Device familiarity Updated: " . $sth->rows . "\n";
 }
 
+# find the manufacturer name of current device
+sub find_manuf {
+  my $mac = $_[0];
+  $mac =~ tr/a-z:/A-Z/d;
+  $mac = substr($mac, 0, 6);
+  my $manuf;
+  my $sth = $dbh->prepare("SELECT manufacturer FROM oui
+	  		   WHERE mac_addr = ?");
+  $sth->execute($mac) or print "Could not find device manufacturer\n";
+  if ( $manuf = $sth->fetchrow_array ) {
+    return $manuf;
+  }  
+  return "N/A";
+}
+
 # add device to devices table;
 sub add_dev {
   my $dev = $_[0];
-  my $sth = $dbh->prepare("INSERT INTO devices (mac_addr, familiar) 
-	                 VALUES (?, 1)");
-  $sth->execute($dev) or print "Has already been put in\n";
+  my $manuf_name = find_manuf($dev);
+  my $sth = $dbh->prepare("INSERT INTO devices (mac_addr, familiar, manufacturer) 
+	                 VALUES (?, 1, ?)");
+  $sth->execute($dev, $manuf_name) or print "Has already been put in\n";
   #print "Number of rows added: " . $sth->rows . "\n";
 }
 
@@ -52,19 +67,16 @@ sub find_dev {
 	                  mac_addr = ?");
   $sth->execute($dev) or return 0;
   if ( ($mac, $time) = $sth->fetchrow_array ) {
-	  #print "Found Device: $mac  $time \n";
+	  #print "Found Device: $mac\n";
 	  return 1;
   }
   return 0;
 }
 
-# define device constants
-my @devices = `sudo tcpdump -i mon1 -e -s 256 type mgt subtype probe-req -c 10 2>&1`; 
-my $num_dev = 0;
-chomp @devices;
 
 # checking for results from tcpdump to see which responses are from devices
 # with valid MAC addresses 
+my $num_dev = 0;
 sub containsMAC {
   my @devinfo = @_;
   my $found = 0;
@@ -82,6 +94,11 @@ sub containsMAC {
   }
 }      
 
+# EXECUTE COMMAND (tcpdump) 
+my @devices = `sudo tcpdump -i mon1 -e -s 256 type mgt subtype probe-req -c 10 2>&1`; 
+chomp @devices;
+
+# parse through results
 foreach my $device ( @devices ) {
   my @arr1 = split(' ', $device); 
   # check if line contains timestamp, if so, it has MAC addr
